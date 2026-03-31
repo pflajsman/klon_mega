5ae10972dda7136f0e13aee33a4faa7f05ae4d8f
Pavel Flajsman <PavelF2@kontent.ai>
2026-03-31 13:10:08 +0200

    Remove secrets from tracking

--- arduino_secrets.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARDUINO_SECRETS_H
-#define ARDUINO_SECRETS_H
-
-// MQTT Broker credentials
-// Replace with your actual MQTT username and password
-#define SECRET_MQTT_USER "your_mqtt_username"
-#define SECRET_MQTT_PASS "your_mqtt_password"
-
-#endif
